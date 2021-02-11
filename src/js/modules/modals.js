5e92e0021d28cbdf12c5c6bf72cba25e45463e67
Efimova Natalia <>
2021-02-11 12:32:53 +0300

    Модальные окна завершены

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,13 +1,21 @@
 const modals = () => {
-    function bindModal(trigger, modal, close) {
-        trigger.addEventListener('click', (e) => {
-            if (e.target) {
-                e.preventDefault();
-            }
+    function bindModal(triggerSelector, modalSelector, closeSelector) {
+        const trigger = document.querySelectorAll(triggerSelector),
+              modal = document.querySelector(modalSelector),
+              close = document.querySelector(closeSelector);
 
-            modal.style.display = "block";
-            document.body.style.overflow = "hidden";
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+    
+                modal.style.display = "block";
+                document.body.style.overflow = "hidden";
+            });
         });
+       
 
         close.addEventListener('click', () => {
             modal.style.display = "none";
@@ -20,15 +28,18 @@ const modals = () => {
                 document.body.style.overflow = "";
             }
         });
-
-          
     }
 
-    const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
-        modalEngineer = document.querySelector('.popup_engineer'),
-        modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+    function showModalByTime (selector, time) {
+        setTimeout(function() {
+            document.querySelector(selector).style.display = 'block';
+            document.body.style.overflow = "hidden";
+        }, time);
+    }
     
-    bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+    showModalByTime('.popup', 3000);
 
 };
 
